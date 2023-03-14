0596b63608487c43de5e9883ac59f1d05707685a
Mateusz Wozniak <ennobler@gmail.com>
2023-03-14 14:06:32 +0100

    Animation - short refactor

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,12 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
 
-window.addEventListener("load", (event) => {
-    var titleElement = document.querySelector("main > div > h1");
+
+function animate(selector) {
+    let interval = null;
+    var titleElement = document.querySelector(selector);
     let iteration = 0;
-
     clearInterval(interval);
-
     interval = setInterval(() => {
         titleElement.innerText = titleElement.innerText
             .split("")
@@ -15,39 +14,18 @@ window.addEventListener("load", (event) => {
                 if (index < iteration) {
                     return titleElement.dataset.value[index];
                 }
-
-                return letters[Math.floor(Math.random() * 26)]
+                return letters[Math.floor(Math.random() * 26)];
             })
             .join("");
-
         if (iteration >= titleElement.dataset.value.length) {
             clearInterval(interval);
         }
-
         iteration += 1 / 6;
     }, 30);
+}
 
-    var logoElement = document.querySelector(".logo__text");
-    let logoInterval = 0;
 
-    clearInterval(logoInterval);
-
-    logoInterval = setInterval(() => {
-        logoElement.innerText = logoElement.innerText
-            .split("")
-            .map((letter, index) => {
-                if (index < iteration) {
-                    return logoElement.dataset.value[index];
-                }
-
-                return letters[Math.floor(Math.random() * 26)]
-            })
-            .join("");
-
-        if (iteration >= logoElement.dataset.value.length) {
-            clearInterval(logoInterval);
-        }
-
-        iteration += 1 / 6;
-    }, 30);
+window.addEventListener("load", (event) => {
+    animate("main > div > h1");
+    animate(".logo__text");
 });
